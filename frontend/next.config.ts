78d97eccc20d0caf4092865c9eb2bc0177e197ae
adrianmcmains <adrianmcmains@gmail.com>
2025-04-08 16:00:56 +0300

    Auto commit: 2025-04-08 16:00:56

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,15 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:8080/api/:path*',
-      },
-    ];
-  },
-}
+import type { NextConfig } from "next";
 
-module.exports = nextConfig
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
